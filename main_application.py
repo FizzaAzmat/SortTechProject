1e46707b95e9675108c43a4ba2cfb80ceee9d832
FizzaAzmat <155991451+FizzaAzmat@users.noreply.github.com>
2024-01-08 22:11:20 +0500

    main_application.py

---
+++ main_application.py
@@ -0,0 +1,14 @@
+
+import tkinter as tk
+import random
+from gui_components import drawData
+from sorting_algorithms import bubble_sort
+
+# Main window setup
+root = tk.Tk()
+root.title('Sorting Algorithm Visualization')
+root.maxsize(900, 600)
+root.config(bg='black')
+
+
+root.mainloop()
