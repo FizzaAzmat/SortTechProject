ca30b0dc6de5103343e2a6ba5cbdbc82aaf9450b
FizzaAzmat <155991451+FizzaAzmat@users.noreply.github.com>
2024-01-08 22:12:10 +0500

    gui_components.py

---
+++ gui_components.py
@@ -0,0 +1,20 @@
+
+
+import tkinter as tk
+from tkinter import ttk
+
+# GUI layout
+def drawData(canvas, data, colorArray):
+    canvas.delete("all")
+    c_height = 380
+    c_width = 600
+    x_width = c_width / (len(data) + 1)
+    offset = 30
+    spacing = 10
+    normalizedData = [i / max(data) for i in data]
+    for i, height in enumerate(normalizedData):
+        x0 = i * x_width + offset + spacing
+        y0 = c_height - height * 340
+        x1 = (i + 1) * x_width + offset
+        y1 = c_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
